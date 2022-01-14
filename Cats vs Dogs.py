31adb381fabf7e6ff80fac8966da8c5fa475086e
Skyking2 <75968450+Skyking2@users.noreply.github.com>
2022-01-14 09:44:26 +0300

    Update Cats vs Dogs.py

--- Cats vs Dogs.py
+++ Cats vs Dogs.py
@@ -1,3 +1,4 @@
+#Import necessary libraries
 import cv2
 import numpy as np
 import os
@@ -6,5 +7,5 @@ from random import shuffle
 TRAIN_DIR = 'FILL_ME'
 TEST_DIR = 'FILL_ME'
 IMG_SIZE = 50
-LR = 1e-3
+LR = 1e-3 #Learning rate
 
